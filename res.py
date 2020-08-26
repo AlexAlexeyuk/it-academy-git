206c6b6f24eacef932ed6f8369f0ca873d7dd112
AlexAlexeyuk <65073437+AlexAlexeyuk@users.noreply.github.com>
2020-08-26 13:56:38 +0300

    Add files via upload

--- res.py
+++ res.py
@@ -1,10 +1,18 @@
+import numpy as np
+
 with open('matrix_1.txt', 'r') as m_1:
-    array_1 = list(m_1.readline())
-    
-    
+    array_1 = m_1.readline().replace(" ", "")
+    arr_1 = eval(array_1.split()[0])
+
 with open('matrix_2.txt', 'r') as m_2:
-    array_2 = list(m_2.readline())
+    array_2 = m_2.readline().replace(" ", "")
+    arr_2 = eval(array_2.split()[0])
+res = list()
+for i in range(5):
+    for j in range(5):
+        res.append(arr_1[i][j] * arr_2[i][j])
 
-    
-
-print(array_1, type(array_1))
+final_res = np.array(res)
+final_res = final_res.reshape([5, 5])
+with open('result.txt', 'w') as r:
+    r.write(str(final_res))
