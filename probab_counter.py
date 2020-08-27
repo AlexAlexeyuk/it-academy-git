5c804534a2c5b7ad7a306aea9377fe82650407f9
AlexAlexeyuk <65073437+AlexAlexeyuk@users.noreply.github.com>
2020-08-27 18:42:57 +0300

    Update probab_counter.py

--- probab_counter.py
+++ probab_counter.py
@@ -9,7 +9,7 @@ class Probability_counter:
     self.p2 = 0.5
     self.p1 = 0.5
 
-  def probabil(self):
+  def probability(self):
     amount_of_blue_balls_1 = (int(input("Введите сколько синих шариков в первом ящике: ")))
     amount_of_white_balls_1 = (int(input("Введите сколько белых шариков в первом ящике: ")))
     amount_of_blue_balls_2 = (int(input("Введите сколько синих шариков во втором ящике: ")))
@@ -17,13 +17,19 @@ class Probability_counter:
     try:
       pb1 = amount_of_blue_balls_1 / (amount_of_blue_balls_1 + amount_of_white_balls_1)
     except ZeroDivisionError:
-      print(" мы не можем посчитать вероятность если нет шаров")
+      pb1 = 0
     try:
       pb2 = amount_of_blue_balls_2 / (amount_of_blue_balls_2 + amount_of_white_balls_2)
     except ZeroDivisionError:
-      print(" мы не можем посчитать вероятность если нет шаров")
-    return   (self.p1 * pb1) / (self.p1 * pb1 + self.p2 * pb2)
+      pb2 = 0
+    try:
+      return  ("Вероятность составляет: " + str(((self.p1 * pb1) / \
+              (self.p1 * pb1 + self.p2 * pb2)*100)) + " процентов")
+    except ZeroDivisionError:
+      return ("Коль нет шаров ни в одном из ящиков, так " 
+       "и вероятности рассчитать не сумеешь ты")
+
    
 
-inst_ = Probability_counter()
-print(inst_.probabil())
+inst1 = Probability_counter()
+print(inst1.probability())
