6ad636e65fc640ec3bdbe2856c5b7c0ece020bf1
AlexAlexeyuk <65073437+AlexAlexeyuk@users.noreply.github.com>
2020-09-03 21:46:09 +0300

    update_symbols

--- desc_grad.py
+++ desc_grad.py
@@ -1,3 +1,4 @@
+from sympy import Symbol
 def determ_amount_var():
   """ Функция.
   возвращает количество пременных во введённой функции
@@ -5,14 +6,15 @@ def determ_amount_var():
   f = input('Введите функцию: ')
   dct = {}
   lst = []
-  for i in f.replace("e", '').split(): # убираем e, т.к. знаем, что это не переменная
+  for i in f.replace("e", '').replace("pi", "").split(): # убираем e, т.к. знаем, что это не переменная
     dct[i] = dct.get(i, 0) + 1 # делаем словарь с ключами, чтобы получить только уникальные ключи
   for i in dct.keys():
     if i[0].isalpha(): 
-      lst.append(i) # далее запихиваем в список только те ключи, которые начинаются с буквы ( чтобы получить кол-во переменных в нашей функции)
+      lst.append(Symbol(i)) # далее запихиваем в список только те ключи, которые начинаются с буквы ( чтобы получить кол-во переменных в нашей функции)
     else:
       pass
-  return len(lst)
+  variables = tuple(lst)
+  return len(lst), variables
 
 
 print(determ_amount_var())
