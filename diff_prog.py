f7b9f1376a90cdc4b01d72b680ae6ec8fb92b8e8
AlexAlexeyuk <65073437+AlexAlexeyuk@users.noreply.github.com>
2020-09-03 22:02:07 +0300

    Update diff_prog.py

--- diff_prog.py
+++ diff_prog.py
@@ -25,3 +25,22 @@ for i in ('x', 'y', 'z', 'b'):
   lst.append(Symbol(i))
 
 print(lst)
+
+
+
+from numpy import sin, cos, arange, linspace, pi, zeros
+import scipy.optimize as opt    
+
+def dG(thetaf, psi, gamma):
+    return 0.35*(cos(psi))**2*(2*sin(3*thetaf/2+2*gamma)+(1+4*sin(gamma)**2)*sin(thetaf/2)-sin(3*thetaf/2))+sin(psi)**2*sin(thetaf/2)    
+
+nt = 100 
+np = 100 
+gamma = linspace(0, pi/2, nt) 
+psi = linspace(0, pi/2, np) 
+x = zeros((nt, np)) 
+for i, theta in enumerate(gamma):
+    for j, phi in enumerate(psi):
+        print('i = %d, j = %d') %(i, j) 
+        g = lambda thetaf: dG(thetaf,phi,theta) 
+        x[i,j] = opt.brenth(g,-pi/2,pi/2)
