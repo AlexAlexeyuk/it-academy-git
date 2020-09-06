39c451c70ea8a40ccab905fc3e67523bd9cd899e
AlexAlexeyuk <65073437+AlexAlexeyuk@users.noreply.github.com>
2020-09-06 15:46:13 +0300

    Update desc_grad.py

--- desc_grad.py
+++ desc_grad.py
@@ -1,4 +1,6 @@
-from sympy import Symbol, symbols
+from sympy import Symbol, symbols, diff
+import numpy as np
+import random
 
 def determ_amount_var():
   """ Функция.
@@ -16,21 +18,28 @@ def determ_amount_var():
     else:
       pass
   variables = lst
-  return [len(lst), variables, [Symbol(f)]]
+  return [len(lst), variables, f]
 
 
 # x ** 2 + y ** 3 + z ** 4
-print(determ_amount_var())
-c = determ_amount_var()
-amount_of_var = c[0]
-variabl = c[1]
-funci = c[2] 
+data_form_input = determ_amount_var()
+amount_of_var = data_form_input[0]
+variables = tuple(data_form_input[1])
+variabl = list(variables)
+function_for_paste = data_form_input[2] 
+print('Внесите в аргументы искомой функции f{0}'.format(variables))
 
-def Func(variabl):
+
+def f(x, y, z):
+    return eval(function_for_paste)
+
+def derivative(h = .01, lambda_ = .01, *args):
+   lst_of_values  =  [random.randint(2, 5) for val in variables]
+   dct_for_val = {val: random.randint(2, 5) for val in variables}
+   return f(*lst_of_values)
     
-    return Symbol(str(funci).replace("[", "").replace("]", ""))
-
-print(Func(variabl))
     
-
-
+   
+    
+    
+print(derivative())
