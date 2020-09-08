d58ea23f33c87ee55064888feac43db46ff271cc
AlexAlexeyuk <65073437+AlexAlexeyuk@users.noreply.github.com>
2020-09-08 13:31:23 +0300

    Update var_of_extract_fun.py

--- var_of_extract_fun.py
+++ var_of_extract_fun.py
@@ -9,20 +9,20 @@ def determ_amount_var():
   
   возвращает количество пременных во введённой функции
   """
-  global function
+  
   function = input('Введите функцию:F(.....): ')
   dct = {}
   fun = ''
   list_of_var = []
   for i in function: # убираем e, т.к. знаем, что это не переменная
     if i.isalpha():
-      fun = fun + i
+      fun = fun + i # формируем строку с вероятными синволами
     fun = fun.replace('log', '').replace('cos', '').replace('sin', '').\
-    replace('e', '').replace('pi', '')
+    replace('e', '').replace('pi', '') #убираем разного рода функции и переменные
   for i in fun:
     dct[i] = dct.get(i, 0) + 1 # делаем словарь с ключами, чтобы получить только уникальные ключи
   for i in dct.keys():
-    list_of_var.append(Symbol(i))
+    list_of_var.append(Symbol(i))  # формируем список символов
   return tuple(list_of_var)
 
 
@@ -30,4 +30,8 @@ def determ_amount_var():
 # x ** 2 - y ** 3
 # 10 * x ** 2 + y ** 2
 variables = determ_amount_var()
-print('Внесите в аргументы искомой функции f{0}'.format(variables))
+print('Внесите в аргументы искомой функции f{0}'.format(variables)) # просим человека внести аргументы функции
+
+
+def f(x, y, z):
+    return eval(function)
