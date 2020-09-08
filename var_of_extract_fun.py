3470a426a83ad7cc0fe86cb32bd31993d7e9bb61
AlexAlexeyuk <65073437+AlexAlexeyuk@users.noreply.github.com>
2020-09-08 23:09:34 +0300

    Update var_of_extract_fun.py

--- var_of_extract_fun.py
+++ var_of_extract_fun.py
@@ -15,25 +15,24 @@ def determ_amount_var():
   dct = {}
   fun = ''
   list_of_var = []
-  for i in function: # убираем e, т.к. знаем, что это не переменная
-    if i.isalpha():
-      fun = fun + i # формируем строку с вероятными синволами
+  for char in function: # убираем e, т.к. знаем, что это не переменная
+    if char.isalpha():
+      fun = fun + char # формируем строку с вероятными синволами
     fun = fun.replace('log', '').replace('cos', '').replace('sin', '').\
     replace('e', '').replace('pi', '')#убираем разного рода функции и константы
-  for i in fun:
-    dct[i] = dct.get(i, 0) + 1 # делаем словарь с ключами, чтобы получить только уникальные ключи
-  for i in dct.keys():
-    list_of_var.append(Symbol(i))  # формируем список символов
+  for char in fun:
+    dct[char] = dct.get(char, 0) + 1 # делаем словарь с ключами, чтобы получить только уникальные ключи
+  for char in dct.keys():
+    list_of_var.append(Symbol(char))  # формируем список символов
   return tuple(list_of_var)
 
 
+variables = determ_amount_var()
+print('Внесите в аргументы искомой функции f{0}'.format(variables)) # просим человека внести аргументы функции
+#  костыль, который не получилось преодолеть ни классом, ни декоратором
 # x ** 2 + y ** 3 + z ** 4
 # x ** 2 - y ** 3
 # 10 * x ** 2 + y ** 2
-variables = determ_amount_var()
-print('Внесите в аргументы искомой функции f{0}'.format(variables)) # просим человека внести аргументы функции
-
-
 def f(x, y, z):
     return eval(function)
 
